ec3237adb83d0d5d00418a861d46551c8cc9f977
André Lucas <124939431+AndreLucas02@users.noreply.github.com>
2024-03-26 20:14:32 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* Menu hamburguer ao redimensionar me telas menores */
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "img/close_white_36dp.svg";
+    }
+}
+
+
+/*
+function boasVindas(){
+    alert("Bem vindo ao maior de site de jogos e noticias mobile");
+}*/
